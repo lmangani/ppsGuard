765a8038ca9b3b7d4c0857c598096a3b7860e861
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-12-31 01:23:22 +0100

    relogic

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ ppsguard --max_pps=1000
 ```
 
 ### Options
+* ``--interface``: Ethernet Interface. _(default: eth0)_
 * ``--max_pps``: PPS Treshold. _(default: 1000)_
 * ``--max_cpu``: CPU Treshold, _(default: 2)_
 * ``--message``: Warning Message _(default: "WARNING!")_
--- ppsguard.js
+++ ppsguard.js
@@ -6,8 +6,6 @@ var stats_cpu = new Measured.Gauge(function(){ return os.loadavg(); });
 
 var os = require("os"), fs = require('fs');
 
-var lastR = 0;
-
 var argv = require('minimist')(process.argv.slice(2));
 var interface = argv.interface || "eth0";
 
@@ -15,14 +13,15 @@ var warning = argv.message || "WARNING!";
 var max_pps = argv.max_pps || 1000;
 var max_cpu = argv.max_cpu || 2;
 
+var lastR = 0;
 var getPPS = function(){
-	var RX1 = fs.openSync('/sys/class/net/'+interface+'/statistics/rx_packets', 'r');
-	stats_rx.mark(RX1-lastR);
-	lastR = RX1;
+	var RX1 = fs.readFileSync('/sys/class/net/'+interface+'/statistics/rx_packets','utf8');
+		stats_rx.mark((RX1||0)-(lastR||0));
+		lastR = RX1 || 0;
 }
 
 setInterval(function () {
-  getPPS();  
+  getPPS();
   var pps = stats_rx.toJSON().currentRate;
   var cpu = stats_cpu.toJSON()[0]
   if (cpu > max_cpu || pps > max_pps) {
